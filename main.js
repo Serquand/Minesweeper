750bb4c09b9a6dcccb9b3ebf39475bd1f31204bc
Serquand <esteban.vincent@efrei.net>
2022-09-21 22:43:35 +0200

    Adding style for game

--- main.js
+++ main.js
@@ -9,5 +9,18 @@ const checkCase = caseIndex => {
 }
 
 for(let i = 0; i < 361; i++) {
-    test[i].addEventListener("click", () => checkCase(i))
+    test[i].addEventListener("click", () => checkCase(i))   
 }
+
+// let compteur = 0;
+
+// for(let i = 0; i < 19; i++) {
+//     for(let j = 0; j < 19; j++) {
+//         if(arrayMine[i][j] == 'M') test[compteur].appendChild(document.createElement("div"))    
+//         else {
+//             test[compteur].innerHTML = arrayMine[i][j]
+//             test[compteur].classList.add("near-" + arrayMine[i][j])
+//         }
+//         compteur++
+//     }    
+// }
