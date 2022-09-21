2ac20615b55e3d2d5aa387aa0d5c3e6c8ac105a1
Serquand <esteban.vincent@efrei.net>
2022-09-21 10:55:14 +0200

    Setup array mine

---
+++ setupGame.js
@@ -0,0 +1,25 @@
+const isInTheTab = (tab, number) => {
+    for(let i = 0; i < tab.length; i++) {
+        if(tab[i] == number) return true
+    }
+    return false
+}
+
+const initializeMineArray = () => {
+    const arrayMine = new Array(0)
+    let i = 0;
+    while(i < 40) {
+        const rdm = Math.floor(Math.random() * 361);
+        if(isInTheTab(arrayMine, rdm)) continue
+        arrayMine.push(rdm)
+        i++        
+    }
+    return arrayMine
+}
+
+const setupMatrix = () => {
+    const arrayMine = initializeMineArray()
+    console.log(arrayMine);
+}
+
+setupMatrix()
